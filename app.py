d45e897e44d0b41b6cde3d5c1a0bc19f46cc85f9
joycejyxl <157575701+joycejyxl@users.noreply.github.com>
2024-04-03 16:37:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,27 @@ import google.generativeai as palm
 import replicate
 import os
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_PgWv9pc0H0ITuiG96E5M6E8jWuFz4Zy1aZqMA"
+os.environ["REPLICATE_API_TOKEN"] = "r8_8077hKjH65S525LxY4HInxTB3B9D9gL07RIIc"
 
-model = {
-    "model": "models/chat-bison-001"
-}
-palm.configure(api_key="AIzaSyCCT1K99BJ1JbLwhCE7qOcQ5KOZcPJ9ZZ4")
+model = {"model": "models/chat-bison-001"}
+palm.configure(api_key="AIzaSyCNTuMxYYZENdgFl43zxllHRQWoj-tEFOU")
 
 app = Flask(__name__)
 
-name=""
-flag=1
+name = " "
+flag = 1
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
-
+           
 @app.route("/main",methods=["GET","POST"])
 def main():
-    global name,flag
-    if flag==1:
+    global name, flag
+    if flag == 1:
         name = request.form.get("q")
-        flag=0
-    return(render_template("main.html",r=name))
+        flag = 0
+    return(render_template("main.html", r = name))
 
 @app.route("/text",methods=["GET","POST"])
 def text():
@@ -34,9 +32,8 @@ def text():
 @app.route("/text_reply",methods=["GET","POST"])
 def text_reply():
     q = request.form.get("q")
-    r = palm.chat(**model, messages=q)
-    return(render_template("text_reply.html",r=r.last))
-
+    r = palm.chat(**model, messages = q)
+    return(render_template("text_reply.html", r = r.last))
 
 @app.route("/image",methods=["GET","POST"])
 def image():
@@ -47,11 +44,8 @@ def image_reply():
     q = request.form.get("q")
     r = replicate.run(
         "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-        input={
-            "prompt": q,
-        }
-    )
-    return(render_template("image_reply.html",r=r[0]))
+        input={"prompt": q,})
+    return(render_template("image_reply.html", r=r[0]))
 
 @app.route("/music",methods=["GET","POST"])
 def music():
@@ -64,10 +58,10 @@ def music_reply():
         "meta/musicgen:7be0f12c54a8d033a0fbd14418c9af98962da9a86f5ff7811f9b3423a1f0b7d7",
         input={
             "prompt": q,
-            "duration":5
+            "duration": 5
         }
     )
-    return(render_template("music_reply.html",r=r))
+    return(render_template("music_reply.html", r=r))
 
 @app.route("/video",methods=["GET","POST"])
 def video():
@@ -80,17 +74,19 @@ def video_reply():
         "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
         input={
             "prompt": q,
-            "num_frames":10
+            "num_frames": 10,
         }
     )
-    return(render_template("video_reply.html",r=r[0]))
+    return(render_template("video_reply.html", r=r[0]))
            
 @app.route("/end",methods=["GET","POST"])
 def end():
     global flag
     print("for gracefully ended")
-    flag=1
-    return(render_template("index.html"))
-    
+    flag = 1
+    return(render_template("end.html"))           
+           
 if __name__ == "__main__":
     app.run()
+           
+
